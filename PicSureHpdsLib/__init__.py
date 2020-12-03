b37767f0e74057d4e7d8c7ef1187185eee87a3ee
Nick Benik <nbenik@gmail.com>
2020-12-03 10:26:22 -0500

    delete bypass objects (forgot one file)

--- PicSureHpdsLib/__init__.py
+++ PicSureHpdsLib/__init__.py
@@ -9,10 +9,9 @@ Top-level package for PIC-SURE HPDS Python Client.
 
 __author__ = """Nick Benik"""
 __email__ = 'nick_benik@hms.harvard.edu'
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 
 from .PicSureHpds import Adapter
-from .PicSureHpds import BypassAdapter
 from .PicSureHpdsAttrList import AttrList
 from .PicSureHpdsAttrListKeys import AttrListKeys
 from .PicSureHpdsAttrListKeyValues import AttrListKeyValues
@@ -20,8 +19,5 @@ from .PicSureHpdsAttrListStudies import AttrListStudies
 from .PicSureHpdsDictionary import Dictionary
 from .PicSureHpdsDictionaryResult import DictionaryResult
 from .PicSureHpdsQuery import Query
-# from .PicSureHpdsAttrListImmutableKeys import AttrListImmutableKeys
-# from .PicSureHpdsAttrListImmutableKeyValues import AttrListImmutableKeyValues
-# from .PicSureHpdsQueryImmutable import ImmutableQuery
 
 
