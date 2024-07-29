696907e7ea18e2d3b511b50a355df27ea869d4a7
ramari16 <ramari16@gmail.com>
2024-07-29 09:05:27 -0400

    ALS-6279: Update python client to use new genomic metadata endpoints (#19)

--- PicSureHpdsLib/PicSureHpds.py
+++ PicSureHpdsLib/PicSureHpds.py
@@ -138,6 +138,15 @@ class HpdsResourceConnection:
                time.sleep(1)
         return self.connection_reference._api_obj().queryResult(self.resource_uuid, query_uuid)
 
+    def genotype_annotations(self):
+        queryJSON = {"query":{"expectedResultType": 'INFO_COLUMN_LISTING'}, "resourceUUID": self.resource_uuid}
+        httpResults = self.connection_reference._api_obj().syncQuery(self.resource_uuid, json.dumps(queryJSON))
+        result = json.loads(httpResults)
+        return result
+
+    def genotype_annotation_values(self, genomicConceptPath, query):
+        return self.connection_reference._api_obj().searchGenomicConceptValues(self.resource_uuid, genomicConceptPath, query)
+
 
 
 class BypassAdapter(Adapter):
