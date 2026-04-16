c76cfcda87e17f090b92915828a1c76088cdef3f
GeorgeC <gcolon021@gmail.com>
2026-04-16 12:39:44 -0400

    feat: export FacetSet from picsure public facade
    Users can now import FacetSet directly: from picsure import FacetSet.
    This is needed for type hints in user code, though most users will
    get a FacetSet from session.facets().

--- tests/unit/test_facade.py
+++ tests/unit/test_facade.py
@@ -11,13 +11,13 @@ class TestPublicFacade:
     def test_session_is_importable(self):
         assert picsure.Session is not None
 
-    def test_platform_is_importable(self):
-        assert picsure.Platform is not None
-
     def test_all_contains_expected_names(self):
-        expected = {"connect", "PicSureError", "Platform", "Session"}
+        expected = {"connect", "PicSureError", "Session", "FacetSet"}
         assert expected.issubset(set(picsure.__all__))
 
     def test_internal_modules_not_in_all(self):
         for name in picsure.__all__:
             assert not name.startswith("_")
+
+    def test_facet_set_is_importable(self):
+        assert picsure.FacetSet is not None
